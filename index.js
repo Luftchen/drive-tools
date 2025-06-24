d1668a7a4396e310e357c222f0b7b9f252cf669c
Luftchen <luftchen@me.com>
2025-06-24 09:00:48 +0900

    Version diff between 0.0.18 and 0.0.20

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 'use strict'
 const child = require('child_process');
 const platform = process.platform
-
-// For Windows
-const Encoding = require('encoding-japanese');
-const SJISToUTF8 = (bytes) => {
-  return Encoding.convert(bytes, {
-    from: 'SJIS',
-    to: 'UNICODE',
-    type: 'string',
-  });
-};
-
 exports.systemPath = (function(type){
 	switch(platform){
 		case "darwin":
@@ -42,7 +31,7 @@ exports.systemPath = (function(type){
 		break;
 	}
 });
-exports.driveList = (function(drive_type = undefined, drive_type_not = undefined){
+exports.driveList = (function(drive_type = undefined){
 	switch(platform){
 		case "darwin":
 			var cmd =" df -H | grep /dev/ | awk '{print $1}'"; // list up volumes
@@ -51,28 +40,24 @@ exports.driveList = (function(drive_type = undefined, drive_type_not = undefined
 				list_len =list.length, 
 				ret = [];
 			for(var i = 0; i < list_len; i++){
-//				cmd = "diskutil info " + list[i].trim() + " | grep -e \"Device Location\" -e \"Mount Point\" -e \"Optical Media Type\" -e \"Media Type\" | sed -e 's/ //g' | awk -F':' '{print $1,$2,$3,$4}'"
-				cmd = "diskutil info " + list[i].trim() + " | grep -e \"Device Location\" -e \"Mount Point\" -e \"Optical Media Type\" -e \"Media Type\" | sed -e 's/ / /g'"
+				cmd = "diskutil info " + list[i].trim() + " | grep -e \"Device Location\" -e \"Mount Point\" -e \"Optical Media Type\" | sed -e 's/ //g' | awk -F':' '{print $1,$2,$3}'"
 				var tmp = child.execSync(cmd).toString().trim(), 
 					tmp_list = tmp.split("\n"), 
 					tmp_list_len = tmp_list.length, 
 					mountPoint = "", 
 					deviceLocation = "",
 					mediaType = "",
-					mediaType2 = "",
 					external = false;
 				for(var j = 0; j < tmp_list_len; j++){
-					var pair = tmp_list[j].split(":");
-					switch(pair[0].replace(/ /g, "")){
-						case "MountPoint": mountPoint = pair[1].trim(); break;
-						case "DeviceLocation": deviceLocation = pair[1].trim(); break;
-						case "OpticalMediaType": mediaType = pair[1].trim(); break;
-						case "MediaType": mediaType2 = pair[1].trim(); break;
+					var pair = tmp_list[j].split(" ");
+					switch(pair[0]){
+						case "MountPoint": mountPoint = pair[1]; break;
+						case "DeviceLocation": deviceLocation = pair[1]; break;
+						case "OpticalMediaType": mediaType = pair[1]; break;
 						default: break;
 					}
 				}
 				external = deviceLocation == "External";
-				if(mediaType == "" && mediaType2 != ""){mediaType = mediaType2;}
 				ret.push({name: mountPoint, desc: deviceLocation, external: external, mediaType: mediaType});
 			} 
 		return ret;
@@ -81,17 +66,11 @@ exports.driveList = (function(drive_type = undefined, drive_type_not = undefined
 			if(drive_type !== undefined){
 				cond = "where drivetype=" + parseInt(drive_type);
 			}
-			if(drive_type_not !== undefined){
-				cond = "where drivetype!=" + parseInt(drive_type_not);
-			}
-			var charcode = "";//"@chcp 932 >nul & cmd /d/s/c ";
-			var result = child.execSync(charcode + "wmic logicaldisk " + cond + " get description,name,volumename /format:csv");
-			result = SJISToUTF8(result).trim();
-			var list = result.replace(/\r\n/g, "\n").replace(/\r/g, "").split("\n"), 
+			var result = child.execSync("wmic logicaldisk " + cond + " get description,name /format:csv").toString().trim(), 
+				list = result.replace(/\r\n/g, "\n").replace(/\r/g, "").split("\n"), 
 				list_len = list.length, 
 				description_index = 0, 
 				name_index = 0, 
-				volume_name_index = 0, 
 				ret = [],
 				external = false;
 			for(var i = 0; i < list_len; i++){
@@ -101,7 +80,6 @@ exports.driveList = (function(drive_type = undefined, drive_type_not = undefined
 						switch(record[j]){
 							case "Description": description_index = j; break;
 							case "Name": name_index = j; break;
-							case "VolumeName": volume_name_index = j; break;
 							default: break;
 						}
 					}
@@ -110,7 +88,7 @@ exports.driveList = (function(drive_type = undefined, drive_type_not = undefined
 					record[description_index].indexOf("ROM") > -1 ||  
 					record[description_index].indexOf("RAM") > -1 ||  
 					record[description_index].indexOf("USB") > -1;
-				ret.push({name: record[name_index], volume_name: record[volume_name_index], desc: record[description_index], external: external});
+				ret.push({name: record[name_index], desc: record[description_index], external: external});
 			}
 		return ret;
 	}
